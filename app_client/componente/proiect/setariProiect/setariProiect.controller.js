b71dea6e4ed09aae1afc45367edae961a4677964
AlexandruSateanu <alex.sateanu@gmail.com>
2018-01-04 23:04:25 +0200

    Setari Proiect titlu

--- app_client/componente/proiect/setariProiect/setariProiect.controller.js
+++ app_client/componente/proiect/setariProiect/setariProiect.controller.js
@@ -1,25 +1,26 @@
 module.exports = function setariProiectCtrl(proiect, $routeParams) {
   var vm = this;
 
-  vm.antetPagina = {
-    titlu: 'Setari Proiect'
-  };
-
   var proiectId = $routeParams.proiectId;
 
-  /* Cere detalii despre proiect si extrage tipul de proiect. */
+  /* Cere detalii despre proiect si extrage tipul si numele proiectului. */
   proiect
     .infoProiect(proiectId)
     .then(function(response) {
       vm.tipProiect = response.data.proiect.tipProiect;
+      vm.numeProiect = response.data.proiect.numeProiect;
+
+      vm.antetPagina = {
+        titlu: 'Setari Proiect - ' + vm.numeProiect
+      };
+
+      vm.setariEtape = true;
+
+      /* Ascundere tab etape daca proiectul este de tip fara etape. */
+      if (vm.tipProiect === '2') {
+        vm.setariEtape = false;
+      }
     }, function(response) {
       return null;
     });
-
-  vm.setariEtape = true;
-
-  /* Ascundere tab etape daca proiectul este de tip fara etape. */
-  if (vm.tipProiect === 2) {
-    vm.setariEtape = false;
-  }
 };
